8978a942bd2631bfc739e2e2cafe1468baa6ebdb
butch0012 <elnathanmburu@gmail.com>
2024-02-04 17:29:24 +0300

    pizza class and name

--- app/models.py
+++ app/models.py
@@ -15,3 +15,9 @@ class Restaurant(db.Model):
     name = db.Column(db.String(50), nullable=False)  # Restaurant name
     address = db.Column(db.String(100), nullable=False)  # Restaurant address
     pizzas = db.relationship('RestaurantPizza', backref='restaurant', lazy=True)  # Relationship with RestaurantPizza model
+
+class Pizza(db.Model):
+    __tablename__ = 'pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)  # Pizza name
